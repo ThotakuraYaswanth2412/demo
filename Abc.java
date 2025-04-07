e40a2c2b9f30b1ec5d8b179eb069e0fc710997bf
Thotakura Yaswanth <thotakurayaswanth1976@gmail.com>
2025-04-07 22:53:02 +0530

    Abc and Xyz are altered and Finally Subtraction.java has constructed

--- Abc.java
+++ Abc.java
@@ -2,9 +2,10 @@ public class Abc
 {
      public static void main(String [] args)
      {
-	System.out.println("Abc class created");
+	
 	for(int i=1;i<=5;i++){
 	System.out.println("Hello");
+	System.out.println("Abc class created");
 	}
      }
 }
--- Xyz.java
+++ Xyz.java
@@ -1,10 +1,11 @@
 public class Xyz
 {
-	public static void main(String [] args)
-	{
-	     int a=20;
-	     int b=40;
-	     int c= a+b;
-	     System.out.println(c);
-	}
+	 private int a;
+     private String b;
+     
+     public Xyz(int a, String b){
+       this.a = a;
+       this.b = b;
+     }
+
 }
