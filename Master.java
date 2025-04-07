2769f25af63cc4ac4d0c54de43b9a61b02c74a0e
Thotakura Yaswanth <thotakurayaswanth1976@gmail.com>
2025-04-07 23:30:32 +0530

    Constructed Master.java

---
+++ Master.java
@@ -0,0 +1,16 @@
+public class Master {
+
+     private int a;
+     private String b;
+     
+     public Master (int a, String b){
+         this.a = a;
+         this.b = b;
+     }
+     public int getInteger(){
+	return a;
+     }
+     public String getString(){
+	return b;
+     }
+}
