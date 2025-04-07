a900a99d35791609150f647feaf0bddbe023a304
Thotakura Yaswanth <thotakurayaswanth1976@gmail.com>
2025-04-07 16:06:36 +0530

    Altered Abc.java And constructed Xyz.java

--- Abc.java
+++ Abc.java
@@ -3,5 +3,8 @@ public class Abc
      public static void main(String [] args)
      {
 	System.out.println("Abc class created");
+	for(int i=1;i<=5;i++){
+	System.out.println("Hello");
+	}
      }
 }
